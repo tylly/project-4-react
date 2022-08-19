75db5c63dcf48a0f02cab3da30c32b3d633018fb
jamestylladams <jamestylladams@gmail.com>
2022-08-19 14:51:52 -0600

    hayden index fix

--- src/components/projects/ShowAllProjects.js
+++ src/components/projects/ShowAllProjects.js
@@ -1,13 +1,10 @@
-import { 
-    useState, 
-    useEffect 
-} from 'react'
-import Card from 'react-bootstrap/Card'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
 // import LoadingScreen from '../shared/LoadingScreen'
-import { getAllProjects } from '../../api/projects'
-import messages from '../shared/AutoDismissAlert/messages'
+import { getAllProjects } from "../../api/projects";
+import messages from "../shared/AutoDismissAlert/messages";
 
 // ShowAllProjects should make a request to the api
 // To get all services
@@ -15,66 +12,68 @@ import messages from '../shared/AutoDismissAlert/messages'
 
 // style for our card container
 const cardContainerStyle = {
-    display: 'flex',
-    flexFlow: 'row wrap',
-    justifyContent: 'center'
-}
+  display: "flex",
+  flexFlow: "row wrap",
+  justifyContent: "center",
+};
 
 const ProjectIndex = (props) => {
-    const [projects, setProjects] = useState(null)
-    const [error, setError] = useState(false)
+  const [projects, setProjects] = useState(null);
+  const [error, setError] = useState(false);
 
-    const { msgAlert } = props
+  const { msgAlert } = props;
 
-    console.log('Props in ShowAllProjects', props)
+  console.log("Props in ShowAllProjects", props);
 
-    useEffect(() => {
-        console.log(props)
-        getAllProjects()
-            .then(res => setProjects(res.data.projects))
-            .catch(err => {
-                msgAlert({
-                    heading: 'Error Getting Services',
-                    message: messages.getProjectsFailure,
-                    variant: 'danger',
-                })
-                setError(true)
-            })
-    }, [])
+  useEffect(() => {
+    console.log(props);
+    getAllProjects()
+      .then((res) => setProjects(res.data.projects))
+      .catch((err) => {
+        msgAlert({
+          heading: "Error Getting Services",
+          message: messages.getProjectsFailure,
+          variant: "danger",
+        });
+        setError(true);
+      });
+  }, []);
 
-    // If services haven't been loaded yet, show a loading message
-    // if (!services) {
-    //     return <LoadingScreen />
-    // } else if (services.length === 0) {
-    //     return <p>No services yet. Better add some.</p>
-    // }
+  // If services haven't been loaded yet, show a loading message
+  // if (!services) {
+  //     return <LoadingScreen />
+  // } else if (services.length === 0) {
+  //     return <p>No services yet. Better add some.</p>
+  // }
 
-    if (error) {
-        return <p>Error!</p>
-    }
-    console.log('here are our projects!', projects)
+  if (error) {
+    return <p>Error!</p>;
+  }
+  console.log("here are our projects!", projects);
 
-    const projectCards = projects.map(project => (
-        <Card style={{ width: '30%', margin: 5}} key={ project._id }>
-            <Card.Header>{ project.name}</Card.Header>
-            <Card.Body>
-                <Card.Text>
-                    <small><em> { project.description }</em></small><br />
-                    <small>{ project.deployment }</small><br />
-                    <small>Location: { project.front_end_repo }</small><br />
-                    <small>${ project.back_end_repo }</small><br />
+  const projectCards = projects.map((project) => (
+    <Card style={{ width: "30%", margin: 5 }} key={project._id}>
+      <Card.Header>{project.name}</Card.Header>
+      <Card.Body>
+        <Card.Text>
+          <small>
+            <em> {project.description}</em>
+          </small>
+          <br />
+          <small>{project.deployment}</small>
+          <br />
+          <small>Location: {project.front_end_repo}</small>
+          <br />
+          <small>${project.back_end_repo}</small>
+          <br />
 
-                    <Link to={`/projects/${project._id}`}>View { project.name }</Link>
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    ))
+          <Link to={`/projects/${project._id}`}>View {project.name}</Link>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  ));
 
-    return (
-        <div style={ cardContainerStyle }>
-            { projectCards }
-        </div>
-    )
-}
+  return <div style={cardContainerStyle}>{projectCards}</div>;
+};
 
-export default ProjectIndex
+export default ProjectIndex;
