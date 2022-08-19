3143c4d7fe46e0a2cf62d8916f3f9832559863ec
jamestylladams <jamestylladams@gmail.com>
2022-08-19 12:31:19 -0600

    compenent start

---
+++ src/api/developers.js
@@ -0,0 +1,49 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+
+// READ => INDEX
+export const getAllDevelopers = () => {
+    return axios(`${apiUrl}/developers`)
+}
+
+// READ => SHOW
+export const getOneProject = (id) => {
+    return axios(`${apiUrl}/developers/${id}`)
+}
+
+// CREATE
+export const createDeveloper = (user, newDeveloper) => {
+    console.log('hit')
+	return axios({
+		url: apiUrl + '/developers',
+		method: 'POST',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+		data: { developer: newDeveloper }
+	})
+}
+// UPDATE
+export const updateDeveloper = (user, updatedDeveloper) => {
+    console.log('this is updatedDeveloper', updatedDeveloper)
+	return axios({
+		url: `${apiUrl}/projects/${updatedDeveloper._id}`,
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+		data: { developer: updatedDeveloper }
+	})
+}
+
+// DELETE
+export const removeDeveloper = (user, developerId) => {
+    return axios({
+        url: `${apiUrl}/developers/${developerId}`,
+        method: 'DELETE',
+        headers: {
+            Authorization: `Token token=${user.token}`,
+        }
+    })
+}
