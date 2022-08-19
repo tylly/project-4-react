884849b342dd5416893740c8569853eab417ce8a
shaialoni <alonishai@gmail.com>
2022-08-19 14:29:58 -0700

    added Dev componenet, created ShowAllDevs comp.

--- src/components/developers/Dev.js
+++ src/components/developers/Dev.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Card } from "react-bootstrap"
 
 const Dev = ({dev, user, msgAlert}) => {
     
@@ -6,6 +7,18 @@ const Dev = ({dev, user, msgAlert}) => {
     return (
         <>
 
+            <Card style={{ width: '30%', margin: 5}} key={ dev._id }>
+            <Card.Header>{ dev.name}</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    <small><em>Name: { dev.name  }</em></small><br />
+                    <small>LinkedIn: { dev.linkedin }</small><br />
+                    <small>Github: { dev.github }</small><br />
+
+                    {/* <Link to={`/projects/${project._id}`}>View { project.name }</Link> */}
+                </Card.Text>
+            </Card.Body>
+        </Card>
         </>
     )
 }
