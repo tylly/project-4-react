d76b301fa02072f5aa371cfcdb997d44f7d13cdb
shaialoni <alonishai@gmail.com>
2022-08-20 16:00:30 -0700

    Created developer form added aws.js to house the api call function to upload a file updated CreateProject.js and PrijectForm.js with the upload to aws function upgraded the app to react 18 and installed Chakra UI

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
