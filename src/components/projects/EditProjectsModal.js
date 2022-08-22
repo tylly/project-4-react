1e6b6522efd2aabd718164a4c02272118e7162d4
jamestylladams <jamestylladams@gmail.com>
2022-08-22 07:54:32 -0600

    prestage

--- src/components/projects/EditProjectsModal.js
+++ src/components/projects/EditProjectsModal.js
@@ -24,6 +24,8 @@ const EditProjectsModal = (props) => {
           };
         });
       };
+
+      
     return (
         <>
             <Modal //show={show} onHide={handleClose}
@@ -33,7 +35,7 @@ const EditProjectsModal = (props) => {
             <ProjectForm 
             project={project}
             handleChange={handleChange}
-            //handleSubmit={handleSubmit}
+            handleSubmit={handleSubmit}
             heading="Update Destination"
             />
         {/* </Modal.Body> */}
