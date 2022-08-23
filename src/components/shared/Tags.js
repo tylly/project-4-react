bfa79ab6be365727a0ddba2e41c5cf5486f5c7c1
jamestylladams <jamestylladams@gmail.com>
2022-08-23 09:56:56 -0600

    dropdown tag work

---
+++ src/components/shared/Tags.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useState, useLocation } from "react";
+import { Dropdown, DropdownButton, Form } from "react-bootstrap";
+import '../../style.css'
+export default function Tags() {
+    const [value, setValue] = useState("React");
+    const [tags, setTags] = useState([])
+    
+    const handleSelect = (e) => {
+      console.log(e);
+      setTags(current => [...current, e]);
+      console.log(tags)
+    };
+
+  let Tags = [
+    "angular",
+    "apple",
+    "adobe",
+    "apache",
+    "axios",
+    "bash",
+    "c",
+    "c-",
+    "c#",
+    "c++",
+    "canvas",
+    "curl",
+    "c shell",
+    "dart",
+    "database",
+    "django",
+    "express",
+    "firebase",
+    "git",
+    "gituhb",
+    "go",
+    "go!",
+    "google",
+    "java",
+    "javascript",
+    "leetcode",
+    "lodash",
+    "mern",
+    "mongodb",
+    "mongoose",
+    "nesl",
+    "net.data",
+    "node",
+    "opal",
+    "python",
+    "python3",
+    "pipenv",
+    "react",
+    "scratch",
+    "script.net",
+    "sql",
+    "swift",
+    "typescript",
+    "visual studio",
+    "vue",
+    "",
+  ];
+
+  let tagDropDownItems = Tags.map((item) => (
+    <Dropdown.Item eventKey={item}>{item}</Dropdown.Item>
+  ));
+
+  return (
+    <>
+    
+        {tagDropDownItems}   
+     
+    </>
+  );
+}
