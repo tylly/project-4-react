2a98e8414dd87b0c67af8b981a7096c2899a18f1
hrmoyes <hrmoyes@gmail.com>
2022-08-24 10:21:28 -0500

    added some more

--- src/components/shared/DeveloperShow.js
+++ src/components/shared/DeveloperShow.js
@@ -4,6 +4,7 @@ import Dev from '../developers/Dev'
 import messages from "../shared/AutoDismissAlert/messages"
 import LoadingChakra from '../shared/LoadingChakra'
 import { Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 
 const DeveloperShowPreview = ({devs, user, msgAlert}) => {
@@ -17,6 +18,9 @@ const DeveloperShowPreview = ({devs, user, msgAlert}) => {
     const devCards = devs.map((dev) =>(
         <div id="developers">
             <h2>{dev.name}</h2>
+            <Link href={dev.github} isExternal>
+                GitHub<ExternalLinkIcon mx='2px' />
+            </Link>
         </div>
     ))
 
