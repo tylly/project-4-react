e50d72d7d8b793f51b8b639b3e60918a03376815
jamestylladams <jamestylladams@gmail.com>
2022-08-26 14:48:59 -0600

    fixing search

--- src/components/projects/ShowAllProjects.js
+++ src/components/projects/ShowAllProjects.js
@@ -5,12 +5,12 @@ import Card from "react-bootstrap/Card";
 import LoadingChakra from "../shared/LoadingChakra";
 import { getAllProjects } from "../../api/projects";
 import messages from "../shared/AutoDismissAlert/messages";
-import { Box } from '@chakra-ui/react'
-import { Link } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Badge, Icon } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
-import { MdSettings } from 'react-icons/md'
+import { Box } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Badge, Icon } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { MdSettings } from "react-icons/md";
 
 import {
   Accordion,
@@ -18,13 +18,13 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
-  ListItem
-} from '@chakra-ui/react'
-import { Center } from '@chakra-ui/react'
-import { Link as RouteLink }  from 'react-router-dom'
-import DeveloperShowPreview from "../shared/DeveloperShow"
+  ListItem,
+} from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+import { Link as RouteLink } from "react-router-dom";
+import DeveloperShowPreview from "../shared/DeveloperShow";
 import ProjectCard from "./ProjectCard";
-import '../../style.css'
+import "../../style.css";
 // ShowAllProjects should make a request to the api
 // To get all services
 // Then display them when it gets them
@@ -37,108 +37,123 @@ const cardContainerStyle = {
   justifyContent: "center",
 };
 
-const ProjectIndex = ({user, msgAlert}) => {
+const ProjectIndex = ({ user, msgAlert }) => {
   const [projects, setProjects] = useState(null);
   const [error, setError] = useState(false);
-  const [updated, setUpdated] = useState(false)
+  const [updated, setUpdated] = useState(false);
 
   // const { msgAlert } = props;
 
   //console.log("Props in ShowAllProjects", props);
 
-    useEffect(() => {
-        console.log('happening shai!')
-        getAllProjects()
-            .then(res => {
-                console.log(res)
-                console.log(res.data)
-                console.log(res.data.projects)
-                setProjects(res.data.projects)})
-            .catch(err => {
-                msgAlert({
-                    heading: 'Error Getting Projects',
-                    message: messages.getProjectsFailure,
-                    variant: 'danger',
-                })
-                setError(true)
-            })
-    }, [updated])
-
-    let handleChange = (e) => {
-      let arr = projects.filter((project) => {
-        if (e.target.value !== "")
-        { 
-          if(project.tags[0].includes(e.target.value) || project.developers[0].name.includes(e.target.value) || project.name.includes(e.target.value)){
-          return project
-        } }
-        else if (e.target.value === "") {
-          console.log("heyyyyyyy")
-          getAllProjects()
-            .then(res => {
-                console.log(res)
-                console.log(res.data)
-                console.log(res.data.projects)
-                setProjects(res.data.projects)})
-            .catch(err => {
-                msgAlert({
-                    heading: 'Error Getting Projects',
-                    message: messages.getProjectsFailure,
-                    variant: 'danger',
-                })
-                setError(true)
-            })
-        }
+  useEffect(() => {
+    console.log("happening shai!");
+    getAllProjects()
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        console.log(res.data.projects);
+        setProjects(res.data.projects);
       })
-      console.log("====================>", e.target.value === "", arr)
-      setProjects(arr)
-    }
+      .catch((err) => {
+        msgAlert({
+          heading: "Error Getting Projects",
+          message: messages.getProjectsFailure,
+          variant: "danger",
+        });
+        setError(true);
+      });
+  }, [updated]);
 
-    // If services haven't been loaded yet, show a loading message
-    if (!projects) {
-        return <LoadingChakra />
-    } else if (projects.length === 0) {
-        return <><p>No projects yet. Better add some.</p>
-        </>
-    }
+  let handleChange = (e) => {
+    let arr = projects.filter((project) => {
+      if (e.target.value !== "") {
+        if (
+          project.tags.find((tag) =>
+            tag.includes(e.target.value.toLowerCase())
+          ) ||
+          project.developers.find((developer) =>
+            developer.name.toLowerCase().includes(e.target.value.toLowerCase())
+          ) ||
+          project.name.toLowerCase().includes(e.target.value.toLowerCase())
+        ) {
+          return project;
+        }
+      } else if (e.target.value === "") {
+        console.log("heyyyyyyy");
+        getAllProjects()
+          .then((res) => {
+            console.log(res);
+            console.log(res.data);
+            console.log(res.data.projects);
+            setProjects(res.data.projects);
+          })
+          .catch((err) => {
+            msgAlert({
+              heading: "Error Getting Projects",
+              message: messages.getProjectsFailure,
+              variant: "danger",
+            });
+            setError(true);
+          });
+      }
+    });
+    console.log("====================>", e.target.value === "", arr);
+    setProjects(arr);
+  };
+
+  // If services haven't been loaded yet, show a loading message
+  if (!projects) {
+    return <LoadingChakra />;
+  } else if (projects.length === 0) {
+    return (
+      <>
+        <p>No projects yet. Better add some.</p>
+      </>
+    );
+  }
 
   if (error) {
     return <p>Error!</p>;
   }
   console.log("here are our projects!", projects);
 
-//   if(!projects.tags){
-//     <Badge>
-//       There are no tags
-//     </Badge>
-//   }
-// const tagSidebar = projects.tags.map((tag)=>(
-      // <ListItem>
-      //    <Badge borderRadius='full' px='2' colorScheme='red'>
-      //     {tag}
-      //   </Badge>
-      // </ListItem>
-// ))
-
+  //   if(!projects.tags){
+  //     <Badge>
+  //       There are no tags
+  //     </Badge>
+  //   }
+  // const tagSidebar = projects.tags.map((tag)=>(
+  // <ListItem>
+  //    <Badge borderRadius='full' px='2' colorScheme='red'>
+  //     {tag}
+  //   </Badge>
+  // </ListItem>
+  // ))
 
   const projectCards = projects.map((project) => (
-
     <ProjectCard
       user={user}
       msgAlert={msgAlert}
-      triggerRefresh={() => setUpdated(prev => !prev)}
+      triggerRefresh={() => setUpdated((prev) => !prev)}
       project={project}
     />
-   
-
   ));
 
   return (
-  <>
-  <input onChange={handleChange} id="search" placeholder={'Search projects'} type={"text"}></input>
-  <div alt="boxContainer" style={cardContainerStyle}>
-    {projectCards}</div>;
+    <>
+      <input
+        onChange={handleChange}
+        id="search"
+        placeholder={"Search projects"}
+        type={"text"}
+      ></input>
+      <div alt="boxContainer" style={cardContainerStyle}>
+        {projectCards}
+      </div>
+      ;
     </>
-  )
+  );
 };
 
 export default ProjectIndex;
